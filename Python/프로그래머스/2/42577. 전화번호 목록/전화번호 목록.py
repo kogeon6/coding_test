43ccf6968077223e741c8c57fcd03dd3ad1011bd
kogeon6 <140686507+kogeon6@users.noreply.github.com>
2026-09-24 13:26:06 +0900

    [level 2] Title: 전화번호 목록, Time: 417.49 ms, Memory: 39.2 MB -BaekjoonHub

--- Python/프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
+++ Python/프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
@@ -1,34 +1,33 @@
-# def solution(phone_book):
-#     answer = True
-#     dict={}
-#     array=[]
-#     for key in phone_book:
-#         if key not in dict:
-#             dict[key] = key
-#     print(dict)
-#     min = 20
-#     for key in dict:
-#         if len(key) < min:
-#             min = len(key)
-#         if len(key) == min:
-#             array.append(key)
-#     print(array)
-#     return answer
-
-
-
-
 def solution(phone_book):
     answer = True
-    phone_book.sort()
-    for i in range(len(phone_book)-1):
-        min_len = min(len(phone_book[i]),len(phone_book[i+1]))
-        for k in range(min_len):
-            if phone_book[i][k]!= phone_book[i+1][k]:
-                break
-            elif k==min_len-1:
+    dict={}
+    array=[]
+    for key in phone_book:
+        if key not in dict:
+            dict[key] = True
+    
+    for key in phone_book:
+        for j in range(1, len(key)):
+            prefix = key[:j]
+            if prefix in dict:
                 return False
+    
     return answer
+
+
+
+
+# def solution(phone_book):
+#     answer = True
+#     phone_book.sort()
+#     for i in range(len(phone_book)-1):
+#         min_len = min(len(phone_book[i]),len(phone_book[i+1]))
+#         for k in range(min_len):
+#             if phone_book[i][k]!= phone_book[i+1][k]:
+#                 break
+#             elif k==min_len-1:
+#                 return False
+#     return answer
 # 완전 탐색 진행 i 랑 i+j
 # i는 phone_book 길이 만큼
 # j는 1부터 phone_book 길이 -i
